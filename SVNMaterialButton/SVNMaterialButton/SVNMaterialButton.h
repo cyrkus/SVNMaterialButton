e2e90a34d40a5ca7349d50733734a6a289dcdb3a
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-07 15:49:56 -0700

    set up xcodeproj

---
+++ SVNMaterialButton/SVNMaterialButton/SVNMaterialButton.h
@@ -0,0 +1,19 @@
+//
+//  SVNMaterialButton.h
+//  SVNMaterialButton
+//
+//  Created by Aaron Dean Bikis on 4/7/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNMaterialButton.
+FOUNDATION_EXPORT double SVNMaterialButtonVersionNumber;
+
+//! Project version string for SVNMaterialButton.
+FOUNDATION_EXPORT const unsigned char SVNMaterialButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNMaterialButton/PublicHeader.h>
+
+
