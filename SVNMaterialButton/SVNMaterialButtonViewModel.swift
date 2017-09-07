4ce1e9324e633e49586cf6c6bc61b3f7882e1b4c
Aaron Bikis <aaron.bikis@gmail.com>
2017-09-07 14:45:44 -0700

    add default model

--- SVNMaterialButton/SVNMaterialButtonViewModel.swift
+++ SVNMaterialButton/SVNMaterialButtonViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SVNTheme
 
 public protocol SVNMaterialButtonViewModel {
   var textColor: UIColor { get }
@@ -15,4 +16,13 @@ public protocol SVNMaterialButtonViewModel {
   var disabledColor: UIColor { get }
   var text: String { get }
   var font: UIFont { get }
+}
+
+public struct SVNMaterialButtonStandardViewModel: SVNMaterialButtonViewModel {
+  public var textColor: UIColor = UIColor.white
+  public var isRounded: Bool = false
+  public var enabledColor: UIColor = SVNTheme_DefaultDark().primaryDialogColor
+  public var disabledColor: UIColor = SVNTheme_DefaultDark().primaryDialogColor.withAlphaComponent(0.75)
+  public var text: String = "Continue"
+  public var font: UIFont = SVNTheme_DefaultDark().mediumHeading
 }
